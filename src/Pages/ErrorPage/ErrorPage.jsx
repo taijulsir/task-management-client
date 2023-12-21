05d261e0b90fbf54a9beb97339195f4560bb5d85
taijulsir <princetaijul123@gmail.com>
2023-12-21 15:49:15 +0600

    added error page with lottie animation

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,21 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+import { Link } from "react-router-dom";
 
 
 const ErrorPage = () => {
     return (
         <div>
-            
+            <div>
+                <Player className=""
+                    autoplay
+                    loop
+                    src="https://lottie.host/e56250aa-a0d7-4dc9-ab37-7bc6d4c3f032/iF7SghvpKH.json"
+                >
+                </Player>
+            </div>
+            <div>
+                <Link to="/" className="btn bg-green-700 text-white text-xl">Go Home</Link>
+            </div>
         </div>
     );
 };
