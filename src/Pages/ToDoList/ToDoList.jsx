8d997cb8154bae9f3f68c6ccae91f2f1a3f75bfb
taijulsir <princetaijul123@gmail.com>
2023-12-22 11:07:31 +0600

    basic setup tanstack query

--- src/Pages/ToDoList/ToDoList.jsx
+++ src/Pages/ToDoList/ToDoList.jsx
@@ -2,11 +2,18 @@ import AuthHook from "../../Hooks/AuthHook";
 
 const ToDoList = () => {
 
-    const {user} = AuthHook()
+    const { user } = AuthHook()
+    const {data:tasks=[],refetch}
 
     return (
-        <div>
-            <h1>Hi,{user?.displayName}</h1>
+        <div className="min-h-screen">
+            <h1 className="text-2xl font-semibold text-white mt-5">Hi,{user?.displayName}⚡👋</h1>
+            <h1 className="text-3xl text-center font-bold text-white ">Complete Your <span className="text-green-600">To-Do-List</span></h1>
+            <div>
+
+            </div>
+
+
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,19 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './Router/router'
 import AuthProvider from './AuthProvider/AuthProvider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+
+
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div className='container mx-auto'>
-      <AuthProvider>
-        <RouterProvider router={router}></RouterProvider>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <RouterProvider router={router}></RouterProvider>
+        </AuthProvider>
+      </QueryClientProvider>
     </div>
   </React.StrictMode>,
 )
